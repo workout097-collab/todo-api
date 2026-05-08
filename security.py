06e630fb6054c94688e014a62e79c635a29a3be1
Vasyl <workout097@gmail.com>
2026-05-08 20:47:00 +0200

    added env support

--- security.py
+++ security.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
@@ -15,11 +18,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 ALGORITHM = "HS256"
 
-
-
-SECRET_KEY = "supersecretkey"
-
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
